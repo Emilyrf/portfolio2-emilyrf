60b84a712d70165af3969c60a0c981f995ab2539
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-03-06 16:39:55 +0100

    Debug form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,62 +1,21 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-
-const schema = yup.object().shape({
-    fullName: yup
-        .string()
-        .required('Name is required')
-        .min(5, 'Minimum length is 5 characters'),
-    subject: yup
-        .string()
-        .required('Subject is required')
-        .min(5, 'Minimum length is 5 characters'),
-    email: yup
-        .string()
-        .required('Email is required')
-        .email('Invalid email address'),
-    message: yup
-        .string()
-        .required('A message is required')
-        .min(10, 'Minimum length is 10 characters'),
-});
-
 export default function Contact() {
-    const {
-        handleSubmit,
-        register,
-        formState: { errors },
-        reset,
-    } = useForm({
-        resolver: yupResolver(schema),
-    });
-
-    const [isSubmitted, setIsSubmitted] = useState(false);
-
-    const onSubmit = async (data) => {
-        setIsSubmitted(true);
-        reset();
-    };
-
     return (
         <section className="h-screen flex justify-center items-center" id="contact">
             <div className='card shrink-0 w-full max-w-md shadow-2xl bg-base-200'>
-                <form className='card-body' onSubmit={handleSubmit(onSubmit)}>
+                <form className='card-body' name="contact" method="POST">
+                <input type="hidden" name="form-name" value="contact" />
                     <h2 className="text-center font-electrolize text-4xl  text-primary textshadow">Contact Me </h2>
                     <div className='form-control'>
-                        <label className='label' htmlFor='fullName'>
+                        <label className='label' htmlFor='name'>
                             Name
                         </label>
                         <input
-                            {...register('fullName')}
-                            id='fullName'
+                            name='name'
+                            required
+                            id='name'
                             type='text'
                             className='input input-bordered'
                         />
-                        {errors.fullName && (
-                            <span className='text-red-600'>{errors.fullName.message}</span>
-                        )}
                     </div>
 
                     <div className='form-control'>
@@ -64,14 +23,12 @@ export default function Contact() {
                             Subject
                         </label>
                         <input
-                            {...register('subject')}
+                            name='subject'
+                            required
                             id='subject'
                             type='text'
                             className='input input-bordered'
                         />
-                        {errors.subject && (
-                            <span className='text-red-600'>{errors.subject.message}</span>
-                        )}
                     </div>
 
                     <div className='form-control'>
@@ -79,14 +36,12 @@ export default function Contact() {
                             Email
                         </label>
                         <input
-                            {...register('email')}
+                            name='email'
+                            required
                             id='email'
                             type='email'
                             className='input input-bordered'
                         />
-                        {errors.email && (
-                            <span className='text-red-600'>{errors.email.message}</span>
-                        )}
                     </div>
 
                     <div className='form-control'>
@@ -94,13 +49,11 @@ export default function Contact() {
                             Message
                         </label>
                         <textarea
-                            {...register('message')}
+                            name='message'
+                            required
                             id='message'
                             className='textarea textarea-bordered'
                         />
-                        {errors.message && (
-                            <span className='text-red-600'>{errors.message.message}</span>
-                        )}
                     </div>
 
                     <div className='text-center'>
@@ -114,5 +67,5 @@ export default function Contact() {
                 </form>
             </div>
         </section>
-    );
+    )
 }
