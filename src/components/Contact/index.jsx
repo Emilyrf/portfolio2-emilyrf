8f37d7ddc449e7922e05710c5b0340ff04c2f488
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-03-06 16:35:09 +0100

    Adds validation

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
@@ -17,159 +20,96 @@ const schema = yup.object().shape({
         .string()
         .required('A message is required')
         .min(10, 'Minimum length is 10 characters'),
-})
-
-
-// export default function Contact() {
-//     return (
-//         <section className="h-screen flex justify-center items-center" id="contact">
-//             <div className='card shrink-0 w-full max-w-md shadow-2xl bg-base-200'>
-//                 <form className='card-body' name="contact" method="POST">
-//                 <input type="hidden" name="form-name" value="contact" />
-//                     <h2 className="text-center font-electrolize text-4xl  text-primary textshadow">Contact Me </h2>
-//                     <div className='form-control'>
-//                         <label className='label' htmlFor='name'>
-//                             Name
-//                         </label>
-//                         <input
-//                             name='name'
-//                             id='name'
-//                             type='text'
-//                             className='input input-bordered'
-//                         />
-//                     </div>
-
-//                     <div className='form-control'>
-//                         <label className='label' htmlFor='subject'>
-//                             Subject
-//                         </label>
-//                         <input
-//                             name='subject'
-//                             id='subject'
-//                             type='text'
-//                             className='input input-bordered'
-//                         />
-//                     </div>
-
-//                     <div className='form-control'>
-//                         <label className='label' htmlFor='email'>
-//                             Email
-//                         </label>
-//                         <input
-//                             name='email'
-//                             id='email'
-//                             type='email'
-//                             className='input input-bordered'
-//                         />
-//                     </div>
-
-//                     <div className='form-control'>
-//                         <label htmlFor='message' className='label'>
-//                             Message
-//                         </label>
-//                         <textarea
-//                             name='message'
-//                             id='message'
-//                             className='textarea textarea-bordered'
-//                         />
-//                     </div>
-
-//                     <div className='text-center'>
-//                         <button
-//                             className='custom-btn text-primary mt-8'
-//                             type='submit'
-//                         >
-//                             Submit
-//                         </button>
-//                     </div>
-//                 </form>
-//             </div>
-//         </section>
-//     )
-// }
-
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+});
 
 export default function Contact() {
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+        reset,
+    } = useForm({
         resolver: yupResolver(schema),
     });
 
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
     const onSubmit = async (data) => {
-        try {
-            const response = await fetch('/', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams(data).toString()
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            console.log('Form submitted successfully!');
-        } catch (error) {
-            console.error('Error submitting form:', error);
-        }
+        setIsSubmitted(true);
+        reset();
     };
 
     return (
         <section className="h-screen flex justify-center items-center" id="contact">
             <div className='card shrink-0 w-full max-w-md shadow-2xl bg-base-200'>
-                <form className='card-body' name="contact" method="POST" onSubmit={handleSubmit(onSubmit)}>
-                    <input type="hidden" name="form-name" value="contact" />
-                    <h2 className="text-center font-electrolize text-4xl text-primary textshadow">Contact Me </h2>
+                <form className='card-body' onSubmit={handleSubmit(onSubmit)}>
+                    <h2 className="text-center font-electrolize text-4xl  text-primary textshadow">Contact Me </h2>
                     <div className='form-control'>
-                        <label className='label' htmlFor='name'>Name</label>
+                        <label className='label' htmlFor='fullName'>
+                            Name
+                        </label>
                         <input
-                            name='name'
-                            id='name'
+                            {...register('fullName')}
+                            id='fullName'
                             type='text'
                             className='input input-bordered'
-                            {...register('name')}
                         />
-                        {errors.name && <span className='text-red-600'>{errors.name.message}</span>}
+                        {errors.fullName && (
+                            <span className='text-red-600'>{errors.fullName.message}</span>
+                        )}
                     </div>
 
                     <div className='form-control'>
-                        <label className='label' htmlFor='subject'>Subject</label>
+                        <label className='label' htmlFor='subject'>
+                            Subject
+                        </label>
                         <input
-                            name='subject'
+                            {...register('subject')}
                             id='subject'
                             type='text'
                             className='input input-bordered'
-                            {...register('subject')}
                         />
-                        {errors.subject && <span className='text-red-600'>{errors.subject.message}</span>}
+                        {errors.subject && (
+                            <span className='text-red-600'>{errors.subject.message}</span>
+                        )}
                     </div>
 
                     <div className='form-control'>
-                        <label className='label' htmlFor='email'>Email</label>
+                        <label className='label' htmlFor='email'>
+                            Email
+                        </label>
                         <input
-                            name='email'
+                            {...register('email')}
                             id='email'
                             type='email'
                             className='input input-bordered'
-                            {...register('email')}
                         />
-                        {errors.email && <span className='text-red-600'>{errors.email.message}</span>}
+                        {errors.email && (
+                            <span className='text-red-600'>{errors.email.message}</span>
+                        )}
                     </div>
 
                     <div className='form-control'>
-                        <label htmlFor='message' className='label'>Message</label>
+                        <label htmlFor='message' className='label'>
+                            Message
+                        </label>
                         <textarea
-                            name='message'
+                            {...register('message')}
                             id='message'
                             className='textarea textarea-bordered'
-                            {...register('message')}
                         />
-                        {errors.message && <span className='text-red-600'>{errors.message.message}</span>}
+                        {errors.message && (
+                            <span className='text-red-600'>{errors.message.message}</span>
+                        )}
                     </div>
 
                     <div className='text-center'>
-                        <button className='custom-btn text-primary mt-8' type='submit'>Submit</button>
+                        <button
+                            className='custom-btn text-primary mt-8'
+                            type='submit'
+                        >
+                            Submit
+                        </button>
                     </div>
                 </form>
             </div>
