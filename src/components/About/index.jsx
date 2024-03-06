08a0b05b2663ef4313ba46fcd9155e31369d2936
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-03-06 15:00:30 +0100

    Debug

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <section className="hero min-h-screen " id="about">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="/src/assets/myavatar.jpeg" className="w-80 h-80 rounded-full shadow-2xl" />
+        <img src="/assets/myavatar.jpeg" alt="Emily" className="w-80 h-80 rounded-full shadow-2xl" />
         <div>
           <p> Hello world! I am</p>
           <h1 className="text-5xl text-primary font-bold">Emily R. Ferreira Front-End development</h1>
