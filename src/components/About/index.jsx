a39e98a6dc5632a4e70158473e89e297fb182ca0
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-03-07 16:35:18 +0100

    Adds about me section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,13 +3,17 @@ import { FaGithub } from "react-icons/fa";
 
 export default function About() {
   return (
-    <section className="hero min-h-screen " id="about">
+    <section className="hero min-h-screen pt-20" id="about">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src="/assets/myavatar.jpeg" alt="Emily" className="w-80 h-80 rounded-full shadow-2xl" />
-        <div>
-          <p> Hello world! I am</p>
-          <h1 className="text-5xl text-primary font-bold">Emily R. Ferreira Front-End development</h1>
-          <p className="py-6">student at Noroff and this is my portfolio.</p>
+        <div className="text-center">
+          <p> Hello world! <br /> I am</p>
+          <h1 className="text-5xl text-primary font-electrolize">Emily R. Ferreira</h1>
+          <p className="py-6 text-xl">Four years ago, I traded the sunny shores of Brazil for the cool Nordic breeze of Norway! <br />
+            Here, studying at Noroff, I embarked on a two-year Front-End development journey where I acquired the skills to proudly call myself a</p>
+          <h2 className="text-5xl text-primary font-electrolize">Front-End developer.</h2>
+          <p className="py-6 text-xl">Now, I showcase my latest projects here.</p>
+
           <a
             href="https://www.linkedin.com/in/emily-rego-ferreira/"
             target="_blank"
