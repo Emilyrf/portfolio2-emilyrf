bcb8e28d277aa5a485fc9b53cafb38b102210172
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-03-07 16:35:26 +0100

    Adds footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <aside>
-                <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+                <p>Emily R. Ferreira - 2024</p>
             </aside>
         </footer>
     );
